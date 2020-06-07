c37ffbbb96909a201123ab6cb6cd136af1a378b7
gui-prado <gui.prado@protonmail.com>
2020-06-07 11:56:49 -0300

    Colocando os métodos

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  selector: "app-tab1",
+  templateUrl: "tab1.page.html",
+  styleUrls: ["tab1.page.scss"]
 })
 export class Tab1Page {
+  public num1: number;
+  public num2: number;
+  public resposta: number;
 
   constructor() {}
 
+  public somar() {
+    this.resposta = this.num1 + this.num2;
+  }
+
+  public subtrair() {
+    this.resposta = this.num1 - this.num2;
+  }
+
+  public multiplicar() {
+    this.resposta = this.num1 * this.num2;
+  }
+
+  public dividir() {
+    this.resposta = this.num1 / this.num2;
+  }
 }
